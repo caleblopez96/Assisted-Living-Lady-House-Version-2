457d2e39f3d7daad716ed7e4c0c75828af2de618
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-07-22 19:52:40 -0700

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,41 @@
+const html = document.querySelector("html")
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const phoneInput = document.querySelector("#phone");
+
+
+// button.addEventListener("click", () => {
+//   path.classList.toggle("svgDay");
+// });
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  html.classList.toggle("remove-scroll")
+}
+
+// form formatting
+
+phoneInput.addEventListener("input", mobileNumberFormFormatting);
+
+function mobileNumberFormFormatting(e) {
+  let input = e.target.value.replace(/\D/g, ''); 
+
+  let formattedInput = '';
+
+  if(input.length > 0) {
+    formattedInput += '('+ input.substring(0, 3);
+  }
+
+  if(input.length >= 4) {
+    formattedInput += ')' + input.substring(3, 6);
+  }
+
+  if(input.length >= 7) {
+    formattedInput += '-' + input.substring(6, 10);
+  }
+
+  e.target.value = formattedInput;
+}
